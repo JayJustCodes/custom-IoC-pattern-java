9366c9c903efa8675e2ba94166d2b062f514ae23
jay <117278599+JayJustCodes@users.noreply.github.com>
2025-02-26 16:10:20 +0100

    refactors app for bean handling with CustomContext

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.context.CustomContext;
+import org.example.context.ICustomContext;
 import org.example.dto.Cat;
 import org.example.dto.Dog;
 import org.example.dto.IAnimal;
@@ -7,14 +9,18 @@ import org.example.dto.IAnimal;
 public class App {
 
     public static void main( String[] args ) {
-        IAnimal duck = () -> System.out.println("Kryo");
+//        IAnimal duck = () -> System.out.println("Kryo");
+//
+//        duck.sound();
+//
+//        IAnimal dog = new Dog();
+//        dog.sound();
+//
+//        IAnimal cat = new Cat();
+//        cat.sound();
 
-        duck.sound();
-
-        IAnimal dog = new Dog();
-        dog.sound();
-
-        IAnimal cat = new Cat();
-        cat.sound();
+        ICustomContext context = new CustomContext("src/main/resources/container.txt");
+        IAnimal animal = context.getBean("animal.class");
+        animal.sound();
     }
 }
